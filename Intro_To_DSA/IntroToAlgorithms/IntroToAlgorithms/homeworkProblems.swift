f9b544d597963382e2a6836d681e7e799f8ef508
Marty Hernandez Avedon <martyavedon@gmail.com>
2016-10-17 22:01:36 -0400

    so we have all but one done. i'm not happy with the prime number algo but it works. i think it would be better if it were done recursively, so we're only checking against primes

--- Intro_To_DSA/IntroToAlgorithms/IntroToAlgorithms/homeworkProblems.swift
+++ Intro_To_DSA/IntroToAlgorithms/IntroToAlgorithms/homeworkProblems.swift
@@ -15,26 +15,70 @@ import Foundation
 
 
 func posZeroNeg(myArr: [Int]) -> (Int, Int, Int) {
-    return (0,0,0)
+    var neg = 0
+    var zeros = 0
+    var pos = 0
+    
+    for num in myArr {
+        if num > 0 {
+            pos += 1
+        }
+        else if num < 0 {
+            neg += 1
+        }
+        else {
+            zeros += 1
+        }
+    }
+    
+    return (neg, zeros, pos)
 }
 
 
 //Given an array of Ints, return the second smallest integer.  Assume every value is unique.  Return nil if the array has less than two elements
 
 func secondSmallest(myArr: [Int]) -> Int? {
-    return nil
+    var sortedArr: [Int]
+    
+    guard myArr.count < 2 else {
+        sortedArr = myArr.sorted{$0 < $1}
+        return sortedArr[1]
+    }
+        return nil
 }
 
 //A number is prime if it is only evenly divisible by 1 and itself.  Write an algorithm that checks if an Int is prime
 
 func isPrime(x: Int) -> Bool {
+    if x % 2 == 0 {
+        return false
+    } else { // i know this test is kind of slow and awful
+        for num in 3..<x {
+            if x % num != 0 {
+                continue
+            } else {
+                return false
+            }
+        }
+    }
     return true
 }
 
 //Write an anglorithm that removes all characters from a String matching an input character
 
 func removeCharacter(s: String, c: Character) -> String {
-    return ""
+    var cleanString = ""
+    let sCharArr = s.characters
+    
+    for char in sCharArr {
+        if char != c {
+            cleanString += String(char)
+        } else {
+            continue
+        }
+    }
+    
+    return cleanString
 }
 
 //Write an algorithm that removes all characters from a String contained within an input array of characters
